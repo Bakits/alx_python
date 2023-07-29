49711eb0124d73121c859007fac1301bb2410947
Bakits <josephinebakita34@gmail.com>
2023-07-29 01:04:31 -0700

    Python import function

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    # declare two variable in different line
+from add_0 import add
+
+
+def main():
     a = 1
     b = 2
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
 
-    # import module add
-    add = __import__("add_0").add
 
-    # print the value
-    print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    main()
