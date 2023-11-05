705256c99bf49ceced997b3369ff243934fc26c2
Bakita <josephinebakita34@gmail.com>
2023-11-05 03:57:47 -0800

    api

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,74 +1,37 @@
-"""
-1-export_to_CSV.py - Export Employee's TODO List Data to CSV
-
-This script fetches information about an employee's TODO list from a REST API and exports it to a CSV file.
-
-Requirements:
-- You must use the requests and csv modules.
-- The script must accept an integer as a parameter, which is the employee ID.
-- The script displays the employee's TODO list progress in the console and exports it to a CSV file.
-
-Example:
-    To export the TODO list for an employee with ID 2:
-    $ python3 1-export_to_CSV.py 2
-
-CSV File Format:
-    "USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
-
-Module Dependencies:
-    - requests: Used for making HTTP requests to the API.
-    - csv: Used for working with CSV files.
-
-Functions:
-    - get_employee_todo_progress(employee_id): Fetches and displays employee TODO list progress.
-        - Parameters:
-            - employee_id (int): The ID of the employee to fetch data for.
-
-Usage:
-    Run this script with an employee ID as a command-line argument to fetch and export the data.
-
-"""
-
 import csv
 import requests
 import sys
 
-# Function to fetch tasks for a specific user
-def fetch_tasks(user_id):
-    url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
-    response = requests.get(url)
-    tasks = response.json()
-    return tasks
+if len(sys.argv) != 2:
+    sys.exit(1)
 
-# Function to export tasks to a CSV file
-def export_to_csv(user_id, tasks):
-    filename = f"{USER_ID}.csv"
-    with open(filename, mode='w', newline='') as csv_file:
-        fieldnames = ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        
-        for task in tasks:
-            writer.writerow({
-                "USER_ID": user_id,
-                "USERNAME": "Antonette",  # You can replace this with any username of your choice
-                "TASK_COMPLETED_STATUS": str(task['completed']),
-                "TASK_TITLE": task['title']
-            })
+employee_id = int(sys.argv[1])
 
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 1-export_to_CSV.py <USER_ID>")
-        sys.exit(1)
+employee_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+todos_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
 
-    user_id = sys.argv[1]
+employee_response = requests.get(employee_url)
+todos_response = requests.get(todos_url)
 
-    try:
-        user_id = int(user_id)
-    except ValueError:
-        print("USER_ID must be an integer.")
-        sys.exit(1)
+if employee_response.status_code != 200 or todos_response.status_code != 200:
+    sys.exit(1)
 
-    tasks = fetch_tasks(user_id)
-    export_to_csv(USER_ID, tasks)
-    print(f"Data exported to {USER_ID}.csv")
+employee_data = employee_response.json()
+todo_data = todos_response.json()
+employee_name = employee_data.get("name", "unknown employee")
+employee_username = employee_data.get("username", "unkown employee")
+
+csv_filename = f"{employee_id}.csv"
+
+with open(csv_filename, mode="w", newline="") as csv_file:
+    csv_writer = csv.writer(csv_file)
+
+    csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+for task in todo_data:
+        task_completed_status = "Completed" if task["completed"] else "Not Completed"
+        csv_writer.writerow([employee_id, employee_username, task_completed_status, task["title"]])
+
+
+with open(csv_filename, 'r') as f:
+     pass
--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,32 +1,40 @@
-#!/usr/bin/python3
-"""Export data in JSON format."""
 import json
 import requests
-from sys import argv
+
+# Function to fetch tasks for a given user ID
+def fetch_user_tasks(user_id):
+    todos_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    todos_response = requests.get(todos_url)
+    if todos_response.status_code != 200:
+        return []  # Return an empty list if there was an error
+    return todos_response.json()
+
+# Function to fetch all user IDs
+def fetch_user_ids():
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    users_response = requests.get(users_url)
+    if users_response.status_code != 200:
+        return []  # Return an empty list if there was an error
+    return [user["id"] for user in users_response.json()]
+
+# Function to export data in JSON format for all tasks from all employees
+def export_todo_all_employees():
+    all_employee_data = {}
+
+    user_ids = fetch_user_ids()
+    for user_id in user_ids:
+        user_tasks = fetch_user_tasks(user_id)
+        employee_name = user_tasks[0]["username"] if user_tasks else "Unknown Employee"
+        
+        #a list of tasks for the current user
+        employee_tasks = [{"username": employee_name, "task": task["title"], "completed": task["completed"]} for task in user_tasks]
+        
+        
+        all_employee_data[user_id] = employee_tasks
+
+    # Export data to JSON file
+    with open("todo_all_employees.json", "w") as json_file:
+        json.dump(all_employee_data, json_file, indent=4)
 
 if __name__ == "__main__":
-    user_id = argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-    username = user_data.get("username")
-
-    tasks_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(user_id)
-    tasks_response = requests.get(tasks_url)
-    tasks_data = tasks_response.json()
-
-    user_tasks = []
-    for task in tasks_data:
-        user_tasks.append(
-            {
-                "username": username,
-                "task": task.get("title"),
-                "completed": task.get("completed"),
-            }
-        )
-
-    todo_dict = {user_id: user_tasks}
-
-    # Export data to JSON
-    with open("{}.json".format(user_id), "w") as json_file:
-        json.dump(todo_dict, json_file)
+    export_todo_all_employees()
